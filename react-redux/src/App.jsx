4a639461ce512afe41e6dec94fa7cf337a3bc35a
Aditya Raj <138434890+Raj-Aditya-27@users.noreply.github.com>
2024-04-17 21:53:54 +0530

    react-redux used

---
+++ react-redux/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Count from './components/Count'
+import {useDispatch} from "react-redux"
+
+function App() {
+  const dispatch=useDispatch();
+  return (
+    <>
+      <button onClick={(e)=>dispatch({type:'INCREMENT'})}>Increment</button>
+      <Count/>
+      <button onClick={(e)=>dispatch({type:'DECREMENT'})}>Decrement</button>
+    </>
+  )
+}
+
+export default App
