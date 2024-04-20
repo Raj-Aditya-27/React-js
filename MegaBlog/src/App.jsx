e1df30679f85092308dd02989ded6a95b750350b
Aditya Raj <138434890+Raj-Aditya-27@users.noreply.github.com>
2024-04-20 14:13:16 +0530

    components added

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -2,13 +2,39 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {useDispatch} from 'react-redux'
+import { useEffect } from 'react'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import Header from "./components/header/Header"
+import Footer from "./components/footer/Footer"
 
 function App() {
-  return (
-    <>
-      <h2>A Blog project with appwrite.</h2>
-    </>
-  )
+
+  const [loading, setloading] = useState(true)
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login(userData))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=>setloading(false))
+  },[])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
---
+++ MegaBlog/src/components/index.js
@@ -0,0 +1,5 @@
+import Container from "./container/Container";
+import Footer from "./footer/Footer";
+import Header from "./header/Header";
+import Logo from "./Logo";
+import LogoutBtn from "./header/LogoutBtn";
