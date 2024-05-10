04260c1e4021cbe74ca268fdace442d70a4e86c5
Aditya Raj <138434890+Raj-Aditya-27@users.noreply.github.com>
2024-05-10 17:16:33 +0530

    mega blog

--- redux-toolkit-todo/src/App.jsx
+++ redux-toolkit-todo/src/App.jsx
@@ -8,7 +8,7 @@ import Todo from "./component/Todo";
 function App() {
   return (
     <>
-      <div className="flex flex-col items-center ">
+      <div className="flex flex-col items-center">
         <h1 className="border w-1/5 p-3 text-lg font-bold">Todo Application</h1>
         <AddTodo />
         <Todo />
